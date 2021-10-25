6bac43b63f8a75c7233c1b1570ebe61009e71c61
Мельченков Никита Михайлович <MelchenkovNM@ctr-pr.ru>
2021-10-25 18:53:48 +0300

    Fix server 4

--- src/services/api/articlesApi.js
+++ src/services/api/articlesApi.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   withCredentials: false,
-  baseURL: 'http://localhost:3001/'
+  // baseURL: 'http://localhost:3001/'
+  baseURL: process.env.PORT
 });
 
 export const articleApi = {

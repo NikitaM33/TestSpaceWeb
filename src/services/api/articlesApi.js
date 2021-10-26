d77169dce3f56a1790aa0ae483a108dd4c25f95c
Мельченков Никита Михайлович <MelchenkovNM@ctr-pr.ru>
2021-10-26 11:39:23 +0300

    ...

--- src/services/api/articlesApi.js
+++ src/services/api/articlesApi.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 const instance = axios.create({
   withCredentials: false,
-  baseURL: 'space-web-test.herokuapp.com/'
+  baseURL: process.env.PORT || 'http://localhost:3001'
 });
 
 export const articleApi = {
   getArticles () {
-    return instance.get('rss');
+    return instance.get('/rss');
   }
 };
